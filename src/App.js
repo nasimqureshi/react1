c38cfce30e45fb4dcb4d435820a67f8050b84f84
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-20 19:46:20 -0600

    commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import { useState } from "react";
+
+ 
+function App() {
+  const [showText, setShowText] = useState(true);
+
+
+  
+  return (
+    <div className='App'>
+      <button onClick = {() => {
+        setShowText(!showText)
+      }}>Show / Hide</button>
+      {showText === true && <h1>Hi my name is Nasim Qureshi</h1>}
+    </div>
+    
+   
+  );
+}
+
+export default App;
