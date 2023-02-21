1b9de2a527e2607b1b3d84ae633336baf0eebd38
nasimqureshi <qureshi.nasim@yahoo.com>
2023-02-20 20:00:09 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import { useState } from "react";
 
  
 function App() {
-  const [showText, setShowText] = useState(true);
+  const [textColor, setTextColor] = useState("black");
 
 
   
   return (
     <div className='App'>
       <button onClick = {() => {
-        setShowText(!showText)
-      }}>Show / Hide</button>
-      {showText === true && <h1>Hi my name is Nasim Qureshi</h1>}
+        // setShowText(!showText)
+        setTextColor(textColor === "black" ?  "red": "black");
+      }}>Changing the color</button>
+       <h1 style={{color: textColor}}>Hi my name is Nasim Qureshi</h1>}
     </div>
     
    
